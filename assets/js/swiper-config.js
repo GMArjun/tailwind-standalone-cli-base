07b3acfe9c8c9333cbae7e89fd086765469b7483
Arjun Pandi <gmarjun100@gmail.com>
2024-04-11 09:55:26 +0530

    some changes in ui

--- assets/js/swiper-config.js
+++ assets/js/swiper-config.js
@@ -1,7 +1,13 @@
 new Swiper(".banner", {
   loop: true,
 
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+
   pagination: {
     el: ".banner-pagination",
+    clickable: true,
   },
 });
---
+++ assets/js/talent-selector.js
@@ -0,0 +1,82 @@
+const talents = [
+  {
+    id: 1,
+    title: "Figurant",
+  },
+  {
+    id: 2,
+    title: "Mannequin",
+  },
+  {
+    id: 3,
+    title: "Acteur",
+  },
+  {
+    id: 4,
+    title: "Comédien",
+  },
+  {
+    id: 5,
+    title: "Model photo",
+  },
+  {
+    id: 6,
+    title: "Influenceurs",
+  },
+  {
+    id: 7,
+    title: "Chanteur / Rappeur",
+  },
+  {
+    id: 8,
+    title: "Dj / Beat maker",
+  },
+  {
+    id: 9,
+    title: "Musicien",
+  },
+  {
+    id: 10,
+    title: "Styliste de mode",
+  },
+  {
+    id: 11,
+    title: "Danseur",
+  },
+  {
+    id: 12,
+    title: "Acrobate / Cascadeur",
+  },
+  {
+    id: 13,
+    title: "Maquilleur",
+  },
+  {
+    id: 14,
+    title: "Photographe",
+  },
+  {
+    id: 15,
+    title: "Vidéaste",
+  },
+  {
+    id: 16,
+    title: "Animateur",
+  },
+  {
+    id: 17,
+    title: "Scénariste",
+  },
+];
+
+$(function () {
+  $("select").selectize({
+    plugins: ["remove_button"],
+    maxItems: null,
+    valueField: "id",
+    labelField: "title",
+    searchField: "title",
+    options: talents,
+    create: false,
+  });
+});
