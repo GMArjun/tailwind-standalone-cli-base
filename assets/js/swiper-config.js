ebe073289c2406d480612624e3874558c8d4c7d8
Arjun Pandi <gmarjun100@gmail.com>
2024-04-18 10:47:23 +0530

    added elements in homepage

--- assets/js/swiper-config.js
+++ assets/js/swiper-config.js
@@ -11,3 +11,20 @@ new Swiper(".banner", {
     clickable: true,
   },
 });
+
+new Swiper(".testimonal", {
+  loop: true,
+
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".testimonal-pagination",
+    clickable: true,
+  },
+});
